b43a7b31e003707706d112143a4d116f8e52b33a
zanzalimit <davide.riccarand@gmail.com>
2024-10-02 15:57:19 +0200

    applicazione base del main

--- main.py
+++ main.py
@@ -11,7 +11,24 @@ from pygame.locals import (
     QUIT,
 )
 
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
 
+while running:
+   
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+   
+    screen.fill("purple")
+
+    pygame.display.flip()
+
+    clock.tick(60)  
+pygame.quit()
 
 
 pygame.init()
@@ -22,6 +39,7 @@ icona = pygame.image.load()
 pygame.display.set_icon(icona)
 clock = pygame.time.Clock()
 running = True
+
 
 while running:
     for event in pygame.event.get():
